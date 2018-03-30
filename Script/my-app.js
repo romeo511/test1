a10c60fde314da6134753355ee9b5d0121828eee
root1 <root1@root.local>
2018-03-30 05:27:12 +0300

    add

---
+++ Script/my-app.js
@@ -0,0 +1,1 @@
+var myApp=new Framework7();var $$=Dom7;var mainView=myApp.addView('.view-main',{dynamicNavbar:true});var view2=myApp.addView('#view-2',{dynamicNavbar:true});var view3=myApp.addView('#view-3',{dynamicNavbar:true});var view4=myApp.addView('#view-4',{dynamicNavbar:true});var view5=myApp.addView('.view-5',{dynamicNavbar:true});
